9f11b2444d4a7225b2781a2cc80251f0ebbef9ea
Shan Torrejos <117603433+shantorrejos@users.noreply.github.com>
2023-09-27 13:46:19 +0800

    Added two functions
    getplayerchoice, and playoneround

--- cc.js
+++ cc.js
@@ -1,8 +1,34 @@
 function getComputerChoice(){
     let randomNumber = Math.floor((Math.random()) * 3);
-    const computerChoice = randomNumber === 0 ? 'Rock' : 
-    randomNumber === 1 ? 'Paper': randomNumber === 2 ? 'Scissors' : 'Error';
+    const computerChoice = randomNumber === 0 ? 'Rock' 
+    : randomNumber === 1 ? 'Paper'
+    : randomNumber === 2 ? 'Scissors' 
+    : 'Error';
+
     return computerChoice;
+}
+
+function getPlayerChoice(){
+    let playerChoice = prompt("unsa man imo?");
+
+    return playerChoice;
+}   
+
+function playOneRound(){
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+    
+    if (playerChoice === 'Paper' && computerChoice ==='Rock'
+     || playerChoice === 'Scissor' && computerChoice === 'Paper'
+     || playerChoice === 'Rock' && computerChoice === 'Scissor') {
+        
+        console.log('Winner!')
+    }else if (playerChoice === computerChoice){
+        console.log('TIE!')
+        playOneRound();
+    }else{
+        console.log('Loser!')
+    }
 }
 
 
